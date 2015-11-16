a9487268e4c9edf6b735e774d4daffc921a8a62c
Simon Nyberg <simon.nyberg@viaplay.com>
2015-11-16 15:21:09 +0100

    Increment with custom value

--- test/counterTest.js
+++ test/counterTest.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe.skip('counter', function() {
+describe('counter', function() {
 	var Counter = require('../lib/counter');
 	var expect = require('chai').expect;
 	var instance;
@@ -12,5 +12,9 @@ describe.skip('counter', function() {
 	it('should increment counter', function() {
 		instance.inc();
 		expect(instance.get().value).to.equal(1);
+	});
+	it('should increment with a provided value', function() {
+		instance.inc(100);
+		expect(instance.get().value).to.equal(100);
 	});
 });
