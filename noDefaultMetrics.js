46c090a17dacd612e291f7bb809f4349d5ad0e32
Oleksandr Kureniov <s@zxc.pp.ua>
2017-04-22 23:12:59 +0300

    add file with disabled default metrics

---
+++ noDefaultMetrics.js
@@ -0,0 +1,18 @@
+/**
+ * Prometheus client
+ * @module Prometheus client
+ */
+
+'use strict';
+
+exports.register = require('./lib/register');
+exports.contentType = require('./lib/register').contentType;
+
+exports.Counter = require('./lib/counter');
+exports.Gauge = require('./lib/gauge');
+exports.Histogram = require('./lib/histogram');
+exports.Summary = require('./lib/summary');
+exports.Pushgateway = require('./lib/pushgateway');
+
+exports.linearBuckets = require('./lib/bucketGenerators').linearBuckets;
+exports.exponentialBuckets = require('./lib/bucketGenerators').exponentialBuckets;
