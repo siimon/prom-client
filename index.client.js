ecdceaa3192ad9576f1f3bbd540fe61ac7b7f03c
Pawel Badenski <pawel.badenski@gmail.com>
2018-04-07 21:27:52 +0100

    support browser

---
+++ index.client.js
@@ -0,0 +1,21 @@
+/**
+ * Prometheus client
+ * @module Prometheus client
+ */
+
+'use strict';
+
+exports.register = require('./lib/registry').globalRegistry;
+exports.Registry = require('./lib/registry');
+exports.contentType = require('./lib/registry').globalRegistry.contentType;
+
+exports.Counter = require('./lib/counter');
+exports.Gauge = require('./lib/gauge');
+exports.Histogram = require('./lib/histogram');
+exports.Summary = require('./lib/summary');
+exports.Pushgateway = require('./lib/pushgateway');
+
+exports.linearBuckets = require('./lib/bucketGenerators').linearBuckets;
+exports.exponentialBuckets = require('./lib/bucketGenerators').exponentialBuckets;
+
+exports.aggregators = require('./lib/metricAggregators').aggregators;
