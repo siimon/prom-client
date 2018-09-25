7bb7002f982885ddfc5db03043e184d55e1a632e
Nowell Strite <nowell@strite.org>
2018-09-25 09:34:51 -0400

    CR feedback.

--- benchmarks/histogram.js
+++ benchmarks/histogram.js
@@ -1,24 +1,85 @@
 'use strict';
 
+const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
 module.exports = setupHistogramSuite;
 
 function setupHistogramSuite(suite) {
 	suite.add(
-		'observe',
-		(client, histogram) => histogram.observe(1, { a: 1, b: 1 }),
-		{ setup }
+		'observe#1 with 64',
+		(client, { histogram }) =>
+			permutations([64], labels => histogram.observe(1, labels)),
+		{ teardown, setup: setup(1) }
+	);
+
+	suite.add(
+		'observe#2 with 8',
+		(client, { histogram }) =>
+			permutations([8, 8], labels => histogram.observe(1, labels)),
+		{ teardown, setup: setup(2) }
+	);
+
+	suite.add(
+		'observe#2 with 4 and 2 with 2',
+		(client, { histogram }) =>
+			permutations([4, 4, 2, 2], labels => histogram.observe(1, labels)),
+		{ teardown, setup: setup(4) }
+	);
+
+	suite.add(
+		'observe#2 with 2 and 2 with 4',
+		(client, { histogram }) =>
+			permutations([2, 2, 4, 4], labels => histogram.observe(1, labels)),
+		{ teardown, setup: setup(4) }
+	);
+
+	suite.add(
+		'observe#6 with 2',
+		(client, { histogram }) =>
+			permutations([2, 2, 2, 2, 2, 2], labels => histogram.observe(1, labels)),
+		{ teardown, setup: setup(6) }
 	);
 }
 
-function setup(client) {
-	const registry = new client.Registry();
+function setup(labelCount) {
+	return client => {
+		const registry = new client.Registry();
 
-	const histogram = new client.Histogram({
-		name: 'histogram',
-		help: 'histogram',
-		labelNames: ['a', 'b'],
-		registers: [registry]
-	});
+		const histogram = new client.Histogram({
+			name: 'histogram',
+			help: 'histogram',
+			labelNames: getLabelNames(labelCount),
+			registers: [registry]
+		});
 
-	return histogram;
+		return { registry, histogram };
+	};
+}
+
+function teardown(client, { registry }) {
+	registry.clear();
+}
+
+function getLabelNames(count) {
+	return letters.slice(0, count);
+}
+
+const flatten = (a, b) => [].concat(...a.map(a => b.map(b => [].concat(a, b))));
+const cartesianProduct = (a, b, ...c) =>
+	b ? cartesianProduct(flatten(a, b), ...c) : a;
+const times = a => Array.from(Array(a)).map((_, x) => x);
+
+function permutations(labelValues, fn) {
+	const labelNames = getLabelNames(labelValues.length);
+	labelValues = labelValues.length > 1 ? labelValues : labelValues.concat(1);
+	const labelValuesArray = labelValues.map(times);
+	const labelValueCombinations = cartesianProduct(...labelValuesArray);
+	const labelCombinations = labelValueCombinations.map(values =>
+		labelNames.reduce((acc, label, i) => {
+			acc[label] = values[i];
+			return acc;
+		}, {})
+	);
+
+	return () => labelCombinations.forEach(labels => fn(labels));
 }
