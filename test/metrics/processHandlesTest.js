f0df6fb925f874923a467cdfd04165788caa5681
Yuriy Vasiyarov <yvasiyarov@ozon.travel>
2019-05-16 18:29:06 +0300

    Fix unit test

--- test/metrics/processHandlesTest.js
+++ test/metrics/processHandlesTest.js
@@ -19,9 +19,16 @@ describe('processHandles', () => {
 
 		const metrics = register.getMetricsAsJSON();
 
-		expect(metrics).toHaveLength(1);
-		expect(metrics[0].help).toEqual('Number of active handles.');
+		expect(metrics).toHaveLength(2);
+
+		expect(metrics[0].help).toEqual(
+			'Number of active handles grouped by handle type.'
+		);
 		expect(metrics[0].type).toEqual('gauge');
-		expect(metrics[0].name).toEqual('nodejs_active_handles_total');
+		expect(metrics[0].name).toEqual('nodejs_active_handles');
+
+		expect(metrics[1].help).toEqual('Total number of active handles.');
+		expect(metrics[1].type).toEqual('gauge');
+		expect(metrics[1].name).toEqual('nodejs_active_handles_total');
 	});
 });
