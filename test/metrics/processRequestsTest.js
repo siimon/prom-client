da970a3680fd7410681393ecf41a9d05dcf9147f
Yuriy Vasiyarov <varyous@gmail.com>
2019-06-01 15:38:00 +0300

    Split process request and process handles by type (#260)
    * Report process handles and requests grouped by type
    
    * return names of new metrics
    
    * replace let with const
    
    * Fix unit test
    
    * fix linter errors
    
    * Update CHANGELOG. Improve metrics documentation
    
    * Fix unit tests

--- test/metrics/processRequestsTest.js
+++ test/metrics/processRequestsTest.js
@@ -19,9 +19,15 @@ describe('processRequests', () => {
 
 		const metrics = register.getMetricsAsJSON();
 
-		expect(metrics).toHaveLength(1);
-		expect(metrics[0].help).toEqual('Number of active requests.');
+		expect(metrics).toHaveLength(2);
+		expect(metrics[0].help).toEqual(
+			'Number of active libuv requests grouped by request type. Every request type is C++ class name.'
+		);
 		expect(metrics[0].type).toEqual('gauge');
-		expect(metrics[0].name).toEqual('nodejs_active_requests_total');
+		expect(metrics[0].name).toEqual('nodejs_active_requests');
+
+		expect(metrics[1].help).toEqual('Total number of active requests.');
+		expect(metrics[1].type).toEqual('gauge');
+		expect(metrics[1].name).toEqual('nodejs_active_requests_total');
 	});
 });
