5423339b34a13c20bc665c24a307ce7ab0656fbf
edvardchen <pigeon73101@gmail.com>
2019-08-28 18:04:46 +0800

    feat: add normalizeLabelName and normalizeMetricName

--- test/validationTest.js
+++ test/validationTest.js
@@ -18,4 +18,26 @@ describe('validation', () => {
 			);
 		});
 	});
+
+	describe('normalize metric', () => {
+		const { normalizeMetricName } = require('../lib/validation');
+		it('transform invalid middle chars to underscore', () => {
+			expect(normalizeMetricName('a&&&&^%b')).toEqual('a_b');
+		});
+
+		it('ignore invalid chars at head', () => {
+			expect(normalizeMetricName('123&&&a-b')).toEqual('a_b');
+		});
+
+		it('preserve digits in middle', () => {
+			expect(normalizeMetricName('a-b12:%')).toEqual('a_b12:_');
+		});
+	});
+
+	describe('normalize label', () => {
+		const normalize = require('../lib/validation').normalizeLabelName;
+		it('preserve digits in middle', () => {
+			expect(normalize('a-b12:%')).toEqual('a_b12_');
+		});
+	});
 });
