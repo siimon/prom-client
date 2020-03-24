641b87e3a312732e5c60ce637c5bfe65eebe751f
Guy Ellis <wildfiction@gmail.com>
2020-03-24 16:51:55 -0700

    docs: add example files with console log output

---
+++ example/counter.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// Counter
+// Single Label
+// Multiple Values
+
+const register = require('..').register;
+
+const Counter = require('..').Counter;
+const c = new Counter({
+	name: 'test_counter',
+	help: 'Example of a counter',
+	labelNames: ['code']
+});
+
+c.inc({ code: 200 });
+console.log(register.metrics());
+
+/*
+# HELP test_counter Example of a counter
+# TYPE test_counter counter
+test_counter{code="200"} 1
+*/
+
+c.inc({ code: 200 });
+console.log(register.metrics());
+
+/*
+# HELP test_counter Example of a counter
+# TYPE test_counter counter
+test_counter{code="200"} 2
+*/
+
+c.inc();
+console.log(register.metrics());
+
+/*
+# HELP test_counter Example of a counter
+# TYPE test_counter counter
+test_counter{code="200"} 2
+test_counter 1
+*/
+
+c.reset();
+console.log(register.metrics());
+
+/*
+# HELP test_counter Example of a counter
+# TYPE test_counter counter
+*/
+
+c.inc(15);
+console.log(register.metrics());
+
+/*
+# HELP test_counter Example of a counter
+# TYPE test_counter counter
+test_counter 15
+*/
+
+c.inc({ code: 200 }, 12);
+console.log(register.metrics());
+
+/*
+# HELP test_counter Example of a counter
+# TYPE test_counter counter
+test_counter 15
+test_counter{code="200"} 12
+*/
+
+c.labels('200').inc(12);
+console.log(register.metrics());
+
+/*
+# HELP test_counter Example of a counter
+# TYPE test_counter counter
+test_counter 15
+test_counter{code="200"} 24
+*/
---
+++ example/gauge.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// Gauge
+// Single Label
+// Multiple Values
+
+const register = require('..').register;
+
+const Gauge = require('../').Gauge;
+const g = new Gauge({
+	name: 'test_gauge',
+	help: 'Example of a gauge',
+	labelNames: ['code']
+});
+
+g.set({ code: 200 }, 5);
+console.log(register.metrics());
+/*
+# HELP test_gauge Example of a gauge
+# TYPE test_gauge gauge
+test_gauge{code="200"} 5
+ */
+
+g.set(15);
+console.log(register.metrics());
+/*
+# HELP test_gauge Example of a gauge
+# TYPE test_gauge gauge
+test_gauge{code="200"} 5
+test_gauge 15
+ */
+
+g.labels('200').inc();
+console.log(register.metrics());
+/*
+# HELP test_gauge Example of a gauge
+# TYPE test_gauge gauge
+test_gauge{code="200"} 6
+test_gauge 15
+ */
+
+g.inc();
+console.log(register.metrics());
+/*
+# HELP test_gauge Example of a gauge
+# TYPE test_gauge gauge
+test_gauge{code="200"} 6
+test_gauge 16
+ */
+
+g.set(22);
+console.log(register.metrics());
+/*
+# HELP test_gauge Example of a gauge
+# TYPE test_gauge gauge
+test_gauge{code="200"} 6
+test_gauge 22
+ */
