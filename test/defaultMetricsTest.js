72bf26805d3d806ddb13b72cf840a60e61548db4
Ryan Koval <ryan@ryankoval.com>
2020-06-11 17:01:12 -0500

    feat: added support for adding labels to default metrics

--- test/defaultMetricsTest.js
+++ test/defaultMetricsTest.js
@@ -69,6 +69,21 @@ describe('collectDefaultMetrics', () => {
 		}
 	});
 
+	it('should apply labels to metrics when configured', () => {
+		const labels = {
+			NODE_APP_INSTANCE: 0,
+		};
+		interval = collectDefaultMetrics({
+			labels,
+		});
+		expect(register.getMetricsAsJSON()).not.toHaveLength(0);
+		register.getMetricsAsJSON().forEach(metric => {
+			metric.values.forEach(metricValue => {
+				expect(metricValue.labels).toMatchObject(labels);
+			});
+		});
+	});
+
 	describe('disabling', () => {
 		it('should not throw error', () => {
 			const fn = function () {
