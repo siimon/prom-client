e337a7ec54b82af2b4a645b6cc9919900973a2e8
Alexander Brandstedt <abrandstedt@spotify.com>
2020-07-08 12:04:03 +0200

    fix eslint issues

--- example/pushgateway.js
+++ example/pushgateway.js
@@ -1,24 +1,26 @@
+'use strict';
+
 const client = require('prom-client');
 
 function run() {
 	const Registry = client.Registry;
 	const register = new Registry();
-	let gateway = new client.Pushgateway('http://127.0.0.1:9091',  [], register);
+	const gateway = new client.Pushgateway('http://127.0.0.1:9091', [], register);
 	const prefix = 'dummy_prefix_name';
 
 	const test = new client.Counter({
 		name: `${prefix}_test`,
 		help: `${prefix}_test`,
-		registers: [register]
+		registers: [register],
 	});
 	register.registerMetric(test);
 	test.inc(10);
 
-	gateway.push({ jobName: prefix }, function (err, resp, body) {
-		console.log(`Error: ${err}`)
-		console.log(`Body: ${body}`)
-		console.log(`Response status: ${resp.statusCode}`)
+	gateway.push({ jobName: prefix }, (err, resp, body) => {
+		console.log(`Error: ${err}`);
+		console.log(`Body: ${body}`);
+		console.log(`Response status: ${resp.statusCode}`);
 	});
 }
 
-run()
+run();
