0a8f9c1018b08e360f81f1ca89dc61e4506aeee2
Alexander Brandstedt <abrandstedt@spotify.com>
2020-07-08 11:50:23 +0200

    replace space with tab

--- example/pushgateway.js
+++ example/pushgateway.js
@@ -1,24 +1,24 @@
 const client = require('prom-client');
 
 function run() {
-    const Registry = client.Registry;
-    const register = new Registry();
-    let gateway = new client.Pushgateway('http://127.0.0.1:9091',  [], register);
-    const prefix = 'dummy_prefix_name';
+	const Registry = client.Registry;
+	const register = new Registry();
+	let gateway = new client.Pushgateway('http://127.0.0.1:9091',  [], register);
+	const prefix = 'dummy_prefix_name';
 
-    const test = new client.Counter({
-        name: `${prefix}_test`,
-        help: `${prefix}_test`,
-        registers: [register]
-    });
-    register.registerMetric(test);
-    test.inc(10);
+	const test = new client.Counter({
+		name: `${prefix}_test`,
+		help: `${prefix}_test`,
+		registers: [register]
+	});
+	register.registerMetric(test);
+	test.inc(10);
 
-    gateway.push({ jobName: prefix }, function (err, resp, body) {
-        console.log(`Error: ${err}`)
-        console.log(`Body: ${body}`)
-        console.log(`Response status: ${resp.statusCode}`)
-    });
+	gateway.push({ jobName: prefix }, function (err, resp, body) {
+		console.log(`Error: ${err}`)
+		console.log(`Body: ${body}`)
+		console.log(`Response status: ${resp.statusCode}`)
+	});
 }
 
 run()
