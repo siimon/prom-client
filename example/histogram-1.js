70602380c68770be3f91d6129a7b3eb556d83552
Waqas Hassan <waqas.hassan@roadmunk.com>
2021-05-15 12:56:26 -0400

    added histogram example with observe method using labels

--- example/histogram-1.js
+++ example/histogram-1.js
@@ -15,6 +15,8 @@ const h = new Histogram({
 h.labels('200').observe(0.4);
 h.labels('200').observe(0.6);
 
+h.obsever({ code: '200' }, 0.4);
+
 console.log(register.metrics());
 
 /*
@@ -28,13 +30,13 @@ test_histogram_bucket{le="0.025",code="200"} 0
 test_histogram_bucket{le="0.05",code="200"} 0
 test_histogram_bucket{le="0.1",code="200"} 0
 test_histogram_bucket{le="0.25",code="200"} 0
-test_histogram_bucket{le="0.5",code="200"} 1
-test_histogram_bucket{le="1",code="200"} 2
-test_histogram_bucket{le="2.5",code="200"} 2
-test_histogram_bucket{le="5",code="200"} 2
-test_histogram_bucket{le="10",code="200"} 2
-test_histogram_bucket{le="+Inf",code="200"} 2
-test_histogram_sum{code="200"} 1
-test_histogram_count{code="200"} 2
+test_histogram_bucket{le="0.5",code="200"} 2
+test_histogram_bucket{le="1",code="200"} 3
+test_histogram_bucket{le="2.5",code="200"} 3
+test_histogram_bucket{le="5",code="200"} 3
+test_histogram_bucket{le="10",code="200"} 3
+test_histogram_bucket{le="+Inf",code="200"} 3
+test_histogram_sum{code="200"} 1.4
+test_histogram_count{code="200"} 3
 
 */
