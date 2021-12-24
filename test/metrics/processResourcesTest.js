3b139f9da2dd41c5cf31c643f9203e41074f97a6
Darshan Sen <darshan.sen@postman.com>
2021-12-24 12:12:31 +0530

    Use process.getActiveResourcesInfo() in process metrics
    We should use process.getActiveResourcesInfo() here because it is a
    public alternative of the private APIs process._getActiveHandles() and
    process._getActiveRequests().
    
    Refs: https://nodejs.org/api/process.html#processgetactiveresourcesinfo
    
    Signed-off-by: Darshan Sen <darshan.sen@postman.com>

---
+++ test/metrics/processResourcesTest.js
@@ -0,0 +1,37 @@
+'use strict';
+
+describe('processRequests', () => {
+	const register = require('../../index').register;
+	const processResources = require('../../lib/metrics/processResources');
+
+	beforeAll(() => {
+		register.clear();
+	});
+
+	afterEach(() => {
+		register.clear();
+	});
+
+	it('should add metric to the registry', async () => {
+		if (typeof process.getActiveResourcesInfo !== 'function') {
+			return;
+		}
+
+		expect(await register.getMetricsAsJSON()).toHaveLength(0);
+
+		processResources();
+
+		const metrics = await register.getMetricsAsJSON();
+
+		expect(metrics).toHaveLength(2);
+		expect(metrics[0].help).toEqual(
+			'Number of active resources that are currently keeping the event loop alive, grouped by async resource type.',
+		);
+		expect(metrics[0].type).toEqual('gauge');
+		expect(metrics[0].name).toEqual('nodejs_active_resources');
+
+		expect(metrics[1].help).toEqual('Total number of active resources.');
+		expect(metrics[1].type).toEqual('gauge');
+		expect(metrics[1].name).toEqual('nodejs_active_resources_total');
+	});
+});
