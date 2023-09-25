f1667d6a82bdc743f648e7322a17677842cfda1f
Yosi Attias <yosy101@gmail.com>
2023-09-25 14:24:48 +0300

    Add benchmarks for counter (same as gauge)

---
+++ benchmarks/counter.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { getLabelNames, labelCombinationFactory } = require('./utils/labels');
+
+module.exports = setupCounterSuite;
+
+function setupCounterSuite(suite) {
+	suite.add(
+		'inc',
+		labelCombinationFactory([], (client, { Counter }, labels) =>
+			Counter.inc(labels, 1),
+		),
+		{ teardown, setup: setup(0) },
+	);
+
+	suite.add(
+		'inc with labels',
+		labelCombinationFactory([8, 8], (client, { Counter }, labels) =>
+			Counter.inc(labels, 1),
+		),
+		{ teardown, setup: setup(2) },
+	);
+}
+
+function setup(labelCount) {
+	return client => {
+		const registry = new client.Registry();
+
+		const Counter = new client.Counter({
+			name: 'Counter',
+			help: 'Counter',
+			labelNames: getLabelNames(labelCount),
+			registers: [registry],
+		});
+
+		return { registry, Counter };
+	};
+}
+
+function teardown(client, { registry }) {
+	registry.clear();
+}
