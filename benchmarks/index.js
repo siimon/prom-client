c9907fa5e245f4ab3d88665e6cb7b546a9ed474f
Simen Bekkhus <sbekkhus91@gmail.com>
2024-06-22 10:03:27 +0200

    chore: migrate from eslint-plugin-node to eslint-plugin-n

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -14,6 +14,6 @@ benchmarks.suite('gauge', require('./gauge'));
 benchmarks.suite('summary', require('./summary'));
 benchmarks.run().catch(err => {
 	console.error(err.stack);
-	// eslint-disable-next-line no-process-exit
+	// eslint-disable-next-line n/no-process-exit
 	process.exit(1);
 });
