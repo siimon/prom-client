ebc1fb966a4a44fdf7b6989d654fda370d09aa69
Jason <jdmarshall@users.noreply.github.com>
2025-07-05 19:25:13 -0700

    Add support for nested Groupers.
    Prep for reworking aggregate()

--- test/utilTest.js
+++ test/utilTest.js
@@ -17,4 +17,72 @@ describe('utils', () => {
 			);
 		});
 	});
+
+	describe('Grouper', () => {
+		const { Grouper } = require('../lib/util');
+
+		it('can be instantiated', () => {
+			const grouper = new Grouper();
+
+			expect(grouper.size).toEqual(0);
+		});
+
+		it('supports same constructor syntax as Map', () => {
+			const grouper = new Grouper([['name', []]]);
+
+			expect(grouper.size).toEqual(1);
+			expect(grouper.has('name')).toBe(true);
+		});
+
+		describe('add()', () => {
+			it('can create new records', () => {
+				const grouper = new Grouper([['name', [2]]]);
+
+				grouper.add('name', 3);
+
+				expect(grouper.size).toEqual(1);
+				expect(grouper.get('name')).toStrictEqual([2, 3]);
+			});
+
+			it('creates separate records for each key', () => {
+				const grouper = new Grouper([['name', [2]]]);
+
+				grouper.add('other', 3);
+
+				expect(grouper.size).toEqual(2);
+				expect(grouper.get('other')).toStrictEqual([3]);
+			});
+		});
+
+		describe('getOrAdd()', () => {
+			it('returns existing values', () => {
+				const grouper = new Grouper([['name', [2, 3]]]);
+				const callback = jest.fn();
+
+				const actual = grouper.getOrAdd('name', callback);
+
+				expect(actual).toStrictEqual([2, 3]);
+				expect(callback).not.toHaveBeenCalled();
+			});
+
+			it('adds on missing record', () => {
+				const grouper = new Grouper([['name', [2, 3]]]);
+				const callback = jest.fn(() => 4);
+
+				const actual = grouper.getOrAdd('blah', callback);
+
+				expect(actual).toStrictEqual(4);
+				expect(grouper.get('blah')).toStrictEqual(4);
+				expect(callback).toHaveBeenCalled();
+			});
+
+			it('defaults to inserting an empty array', () => {
+				const grouper = new Grouper([['name', [2, 3]]]);
+				const actual = grouper.getOrAdd('blah');
+
+				expect(actual).toStrictEqual([]);
+				expect(grouper.get('blah')).toStrictEqual([]);
+			});
+		});
+	});
 });
