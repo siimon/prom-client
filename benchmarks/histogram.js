bcbdaf304d56610dfde140569ea897f741f6c743
Matteo Collina <hello@matteocollina.com>
2025-10-13 19:07:48 +0200

    Switch findBound function to a binary search
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- benchmarks/histogram.js
+++ benchmarks/histogram.js
@@ -4,11 +4,15 @@ const { getLabelNames, labelCombinationFactory } = require('./utils/labels');
 
 module.exports = setupHistogramSuite;
 
+function random () {
+	return Math.random() * 100
+}
+
 function setupHistogramSuite(suite) {
 	suite.add(
 		'observe#1 with 64',
 		labelCombinationFactory([64], (client, { histogram }, labels) =>
-			histogram.observe(labels, 1),
+			histogram.observe(labels, random()),
 		),
 		{ teardown, setup: setup(1) },
 	);
@@ -16,7 +20,7 @@ function setupHistogramSuite(suite) {
 	suite.add(
 		'observe#2 with 8',
 		labelCombinationFactory([8, 8], (client, { histogram }, labels) =>
-			histogram.observe(labels, 1),
+			histogram.observe(labels, random()),
 		),
 		{ teardown, setup: setup(2) },
 	);
@@ -24,7 +28,7 @@ function setupHistogramSuite(suite) {
 	suite.add(
 		'observe#2 with 4 and 2 with 2',
 		labelCombinationFactory([4, 4, 2, 2], (client, { histogram }, labels) =>
-			histogram.observe(labels, 1),
+			histogram.observe(labels, random()),
 		),
 		{ teardown, setup: setup(4) },
 	);
@@ -32,7 +36,7 @@ function setupHistogramSuite(suite) {
 	suite.add(
 		'observe#2 with 2 and 2 with 4',
 		labelCombinationFactory([2, 2, 4, 4], (client, { histogram }, labels) =>
-			histogram.observe(labels, 1),
+			histogram.observe(labels, random()),
 		),
 		{ teardown, setup: setup(4) },
 	);
@@ -41,7 +45,7 @@ function setupHistogramSuite(suite) {
 		'observe#6 with 2',
 		labelCombinationFactory(
 			[2, 2, 2, 2, 2, 2],
-			(client, { histogram }, labels) => histogram.observe(labels, 1),
+			(client, { histogram }, labels) => histogram.observe(labels, random()),
 		),
 		{ teardown, setup: setup(6) },
 	);
